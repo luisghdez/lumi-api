c82a9abc1b371035c5fa1976eb352f53f8a1358c
luisghdez <1gluishernandez@gmail.com.com>
2025-03-24 16:05:32 -0600

    host 0.0.0.0

--- src/server.ts
+++ src/server.ts
@@ -34,8 +34,9 @@ fastify.get("/", async () => {
 // Start the server
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
     console.log("🚀 Server running at http://localhost:3000");
+    
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
